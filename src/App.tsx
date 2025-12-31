560c527d245aead7168a16359fad0d925d4d3cce
Jirawat <nuanlaong_j@su.ac.th>
2026-01-01 04:11:04 +0700

    fix: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import Clock from './components/Clock';
+import Greeting from './components/Greeting';
+import UserCard from './components/UserCard';
 import Welcome from "./components/Welcome";
+import Footer from './components/Footer';
 
 function App() {
-
   return (
-      <div className="App">
-          <Welcome />
-      </div>
+    <div className="App min-h-screen flex flex-col items-center justify-center p-4 md:p-8 w-full">
+      <Welcome />
+      <Greeting />
+      <UserCard />
+      <Clock />
+      <Footer />
+    </div>
   )
 }
 
-export default App
+export default App;
