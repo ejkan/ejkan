e8c31f0af4404f348f8d3d6e8e9892512446b870
Jirawat <nuanlaong_j@su.ac.th>
2026-01-01 02:51:17 +0700

    fix: add pnpm lockfile and update workflow version

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import Clock from './components/Clock';
-import Greeting from './components/Greeting';
-import UserCard from './components/UserCard';
 import Welcome from "./components/Welcome";
-import Footer from './components/Footer';
 
 function App() {
 
   return (
       <div className="App">
           <Welcome />
-          <Greeting />
-          <UserCard />
-          <Clock />
-          <Footer />
       </div>
   )
 }
